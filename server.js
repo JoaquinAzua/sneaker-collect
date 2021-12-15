3f691078846208bc3141418ddb704d13d8ff99e2
Joaquin Azua <joaquin.diego.azua@gmail.com>
2021-12-15 17:16:02 -0600

    nav bar add in, mount methodoverride

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/sneakers');
 
 // This will load env variables
 require('dotenv').config();
@@ -20,10 +21,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
