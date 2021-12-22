d34d1afd32ed6965bbf99b5b50af08f3d73ba6f1
Joaquin Azua <joaquin.diego.azua@gmail.com>
2021-12-21 22:19:56 -0600

    details page functional, style closer towards end mark

--- controllers/collections.js
+++ controllers/collections.js
@@ -7,70 +7,114 @@ module.exports = {
     addToOwned,
     addToWatchList,
     index,
+    show,
 };
 
 function addToOwned(req, res) {
-  Collection.findOne(
-    { user: req.user._id, collectionName: "owned" },
-    async function (err, owned) {
-      let sneaker = await Sneaker.findOne({ styleID: req.params.styleID });
-      if (sneaker) {
-        owned.sneakers.push(sneaker._id);
-      } else {
-        sneaks.getProductPrices(
-          req.params.styleID,
-          async function (err, product) {
-            sneaker = await Sneaker.create({
-              styleID: product.styleID,
-              shoeName: product.shoeName,
-              brand: product.brand,
-            });
-            owned.sneakers.push(sneaker._id);
-          }
-        );
+    Collection.findOne(
+      { user: req.user._id, collectionName: "owned" },
+      async function (err, owned) {
+        let sneaker = await Sneaker.findOne({ styleID: req.params.styleID });
+        if (sneaker) {
+          owned.sneakers.push(sneaker._id);
+          owned.save(function (err) {
+            res.redirect("/collections");
+          });
+        } else {
+          sneaks.getProductPrices(
+            req.params.styleID,
+            async function (err, product) {
+              sneaker = new Sneaker({
+                styleID: product.styleID,
+                shoeName: product.shoeName,
+                brand: product.brand,
+              });
+              sneaker.save(function(err) {
+                owned.sneakers.push(sneaker._id);
+                owned.save(function (err) {
+                  res.redirect("/collections");
+                });
+              })
+            }
+          );
+        }
       }
-      owned.save(function (err) {
-        res.redirect("/sneakers");
-      });
-    }
-  );
-}
+    );
+  }
 
-function addToWatchList(req, res) {
-  Collection.findOne(
-    { user: req.user._id, collectionName: "watchlist" },
-    async function (err, watchlist) {
-      let sneaker = await Sneaker.findOne({ styleID: req.params.styleID });
-      console.log(sneaker)
-      if (sneaker) {
-        watchlist.sneakers.push(sneaker._id);
-      } else {
-        sneaks.getProductPrices(
-          req.params.styleID,
-          async function (err, product) {
-            sneaker = await Sneaker.create({
-              styleID: product.styleID,
-              shoeName: product.shoeName,
-              brand: product.brand,
-            });
-            watchlist.sneakers.push(sneaker._id);
-          }
-        );
+  function addToWatchList(req, res) {
+    Collection.findOne(
+      { user: req.user._id, collectionName: "watchlist" },
+      async function (err, watchlist) {
+        let sneaker = await Sneaker.findOne({ styleID: req.params.styleID });
+        if (sneaker) {
+          owned.sneakers.push(sneaker._id);
+          owned.save(function (err) {
+            res.redirect("/collections");
+          });
+        } else {
+          sneaks.getProductPrices(
+            req.params.styleID,
+            async function (err, product) {
+              sneaker = new Sneaker({
+                styleID: product.styleID,
+                shoeName: product.shoeName,
+                brand: product.brand,
+              });
+              sneaker.save(function(err) {
+                watchlist.sneakers.push(sneaker._id);
+                watchlist.save(function (err) {
+                  res.redirect("/collections");
+                });
+              })
+            }
+          );
+        }
       }
-      watchlist.save(function (err) {
-        console.log(err)
-        res.redirect("/sneakers");
-      });
-    }
-  );
-}
+    );
+  }
+
+// function addToWatchList(req, res) {
+//   Collection.findOne({ user: req.user._id, collectionName: "watchlist" },async function (err, watchlist) {
+//       let sneaker = await Sneaker.findOne({ styleID: req.params.styleID });
+//       console.log(sneaker)
+//       if (sneaker) {
+//         watchlist.sneakers.push(sneaker._id);
+//       } else {
+//         sneaks.getProductPrices(
+//           req.params.styleID,
+//           async function (err, product) {
+//             sneaker = await Sneaker.create({
+//               styleID: product.styleID,
+//               shoeName: product.shoeName,
+//               brand: product.brand,
+//             });
+//             watchlist.sneakers.push(sneaker._id);
+//           }
+//         );
+//       }
+//       watchlist.save(function (err) {
+//         console.log(err)
+//         res.redirect("/sneakers");
+//       });
+//     }
+//   );
+// }
 
 function index(req, res) {
   Collection.findOne({ user: req.user._id, collectionName: "owned" }).populate("sneakers").exec(function (err, owned) {
       Collection.findOne({ user: req.user._id, collectionName: "watchlist" }).populate("sneakers").exec(function (err, watchlist) {
-          console.log(owned);
-          console.log(watchlist);
+        //   console.log(owned);
+        //   console.log(watchlist);
           res.render("collections/index", { owned, watchlist });
         });
     });
+}
+
+
+function show(req, res){
+    sneaks.getProductPrices(req.params.styleID, function(err, products){
+        console.log({products})
+        res.render('sneakers/show', {products})
+    })
 }
--- routes/collections.js
+++ routes/collections.js
@@ -5,10 +5,13 @@ const collectionsCtrl = require('../controllers/collections');
 // GET  "/collections" - Index View
 router.get('/collections', collectionsCtrl.index);
 
-// GET "/owned/:styleid" - adding to owned collection
+// GET "/sneakers/styleID" - Show view (details for a sneaker in their collection)
+router.get('/sneakers/:styleID', collectionsCtrl.show)
+
+// GET "/owned/:styleID" - adding to owned collection
 router.get('/owned/:styleID', collectionsCtrl.addToOwned);
 
-// GET "/watchlist/:styleid" - adding to watchlist collection
+// GET "/watchlist/:styleID" - adding to watchlist collection
 router.get('/watchlist/:styleID', collectionsCtrl.addToWatchList);
 
 module.exports = router;
